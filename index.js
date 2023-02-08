4a1a19aca98c4959ecaf5fb88f3bf4b9cc8c7fa1
K-kibet <charleykibet254@gmail.com>
2023-02-08 14:10:30 +0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000; 
+const hostname = '0.0.0.0';
 const app = express();
-const cors = require('cors');
+const cors = require('cors'); 
 dotenv.config();
 
 const userRoute = require('./routes/user');
@@ -11,9 +15,11 @@ const authRoute = require('./routes/auth');
 const bookRoute = require('./routes/book');
 const postRoute = require("./routes/post");
 const uploadRoute = require("./routes/upload");
-
 app.use(express.json());
 app.use(cors());
+app.use(compression());
+
+app.use(helmet());
 app.use('/images', express.static(__dirname + '/images'));
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -29,6 +35,6 @@ app.use('/api/auth', authRoute);
 app.use('/api/books', bookRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/upload", uploadRoute);
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+app.listen(PORT,hostname, () => {
+    console.log(`listening on port${hostname}:${PORT}`);
 });
